a602214261f4cc503d0440ab2a51121468f18f26
grahn <grahn>
2004-06-16 22:41:35 +0000

    start of setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#
+# $Id: setup.py,v 1.1 2004-06-16 22:41:35 grahn Exp $
+#
+from distutils.core import setup
+
+setup(name = 'geese',
+      version = '1.0',
+      description = 'manipulate coordinates and coordinate systems on bitmapped maps',
+
+      author = 'Jörgen Grahn',
+      author_email = 'jgrahn@algonet.se',
+      license = 'BSD',
+      url = 'http://www.algonet.se/~jgrahn/comp/')
