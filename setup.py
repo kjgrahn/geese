966e4aad866a9fd46e1a4af68f70d0e468552fcc
grahn <grahn>
2004-07-20 22:04:05 +0000

    pull project name and version from CVS/RCS name tag

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 #
-# $Id: setup.py,v 1.3 2004-07-12 21:58:51 grahn Exp $
+# $Id: setup.py,v 1.4 2004-07-20 22:04:05 grahn Exp $
 #
 from distutils.core import setup
 
-setup(name = 'geese',
-      version = '1.0',
+def cvs_says(dollarname='$Name:  $'):
+    import re
+    m = re.match(r'\$'r'Name:\s+(.+?)-(\d+(-\d+)*)\D', dollarname)
+    return m.group(1), m.group(2).replace('-', '.')
+
+name, version = cvs_says()
+
+setup(name = name,
+      version = version,
       description = 'manipulate coordinates and coordinate systems on bitmapped maps',
 
       author = 'Jörgen Grahn',
