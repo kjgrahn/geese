d6a8d139d04ac9363cb44f3a440e3d60e4d5ee24
grahn <grahn>
2010-08-29 22:52:43 +0000

    more pseudo-tests, for plotting

--- test_transform.cc
+++ test_transform.cc
@@ -1,5 +1,5 @@
 /**
- * $Id: test_transform.cc,v 1.4 2010-08-29 21:55:19 grahn Exp $
+ * $Id: test_transform.cc,v 1.5 2010-08-29 22:52:43 grahn Exp $
  *
  * Copyright (c) 2010 Jörgen Grahn
  * All rights reserved.
@@ -34,15 +34,15 @@ namespace {
 namespace transform {
 
     /*
-     *  n              +-----> x
+     *  n             0+-----> x
      *  |	     10|øxx
      * 5|øxx         20|x
      * 4|x	     30|xxx
      * 3|xxx         40|x
      * 2|x	     50|x ø
      * 1|x ø           |
-     *  +-----> e      y
-     *   123
+     * 0+-----> e      y
+     *  0123
      */
     void assert_f(const Transform& T)
     {
@@ -59,32 +59,51 @@ namespace transform {
 	near(T(RT90(3,1)), Pixel(10, 30));
 	near(T(RT90(3,2)), Pixel(20, 30));
 	near(T(RT90(3,3)), Pixel(30, 30));
-
-	near(T(RT90(1,1)), Pixel(10, 50));
     }
 
-    void test_simple()
+    void test_upright()
     {
 	const Transform T(RT90(5, 1), Pixel(10, 10),
 			  RT90(1, 3), Pixel(30, 50));
 	assert_f(T);
     }
 
+    void test_right()
+    {
+	const Transform T(RT90(5, 1), Pixel(50, 50),
+			  RT90(1, 3), Pixel(10, 70));
+	assert_f(T);
+    }
+
+    void test_down()
+    {
+	const Transform T(RT90(5, 1), Pixel(10, 90),
+			  RT90(1, 3), Pixel(-10, 50));
+	assert_f(T);
+    }
+
+    void test_left()
+    {
+	const Transform T(RT90(5, 1), Pixel(-30, 50),
+			  RT90(1, 3), Pixel( 10, 30));
+	assert_f(T);
+    }
+
     void test_ugly()
     {
-	const Transform T(RT90(5, 1), Pixel(10, 10),
-			  RT90(1, 3), Pixel(40, 50));
+	const Transform T(RT90(5, 1), Pixel(50, 0),
+			  RT90(1, 1), Pixel(0, 100));
 	assert_f(T);
     }
 
-    void test_simple_world()
+    void test_upright_world()
     {
 	const Transform T(10,  0,  0,
-			   0,-10, 60);
+			   0, 10,-60);
 	assert_f(T);
     }
 
-    void test_transposed()
+    void notest_transposed()
     {
 	const Transform T(RT90(6449005,1368001), Pixel(10, 10),
 			  RT90(6449001,1368003), Pixel(30, 50));
