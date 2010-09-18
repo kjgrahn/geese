06a123c2f8e4e9c8b79e6e9604aa7ad8d11cffe4
grahn <grahn>
2010-09-18 12:53:00 +0000

    no need to enable tilde expansion

--- globbing.cc
+++ globbing.cc
@@ -1,5 +1,5 @@
 /*
- * $Id: globbing.cc,v 1.1 2010-09-13 22:05:57 grahn Exp $
+ * $Id: globbing.cc,v 1.2 2010-09-18 12:53:00 grahn Exp $
  *
  * Copyright (c) 2010 Jörgen Grahn <grahn+src@snipabacken.se>
  * All rights reserved.
@@ -20,7 +20,7 @@ void glob(const std::string& pattern, std::vector<std::string>& v)
 {
     static glob_t GL;
     glob_t gl = GL;
-    if(!glob(pattern.c_str(), GLOB_TILDE_CHECK, 0, &gl)) {
+    if(!glob(pattern.c_str(), 0, 0, &gl)) {
 	for(char** i = gl.gl_pathv; *i; ++i) {
 	    v.push_back(*i);
 	}
