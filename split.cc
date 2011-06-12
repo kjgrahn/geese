28aff4819c09cfbad0d9527b48adb94aaab1beb8
grahn <grahn>
2011-06-12 19:55:34 +0000

    begin ...

--- split.cc
+++ split.cc
@@ -1,7 +1,20 @@
 /*
- * $Id: split.cc,v 1.1 2011-06-12 19:48:39 grahn Exp $
+ * $Id: split.cc,v 1.2 2011-06-12 19:55:34 grahn Exp $
  *
  * Copyright (c) 2011 Jörgen Grahn <grahn+src@snipabacken.se>
  * All rights reserved.
  */
 #include "split.h"
+
+static
+std::vector<std::string> split(const char* a, const char* b)
+{
+    std::vector<std::string> v;
+    return v;
+}
+
+std::vector<std::string> split(const std::string& s)
+{
+    const char* a = &s[0];
+    return split(a, a+s.size());
+}
